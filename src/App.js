554dcbaef6a429a09c29bb94b94ed143bc882dab
Hmin25 <69612620+Hmin25@users.noreply.github.com>
2022-06-26 15:21:09 +0800

    Update UI - different calculation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { ChakraProvider, Center, Text, Button, Flex } from "@chakra-ui/react"
+import { ChakraProvider, Center, Text, Button, Flex, Box, Grid, Divider } from "@chakra-ui/react"
 import { useState, useEffect } from 'react'
 import Axios from 'axios'
 import FlexBox from './Components/FlexBox';
@@ -9,25 +9,39 @@ function App() {
   const [currentPiValue, setCurrentPiValue] = useState();
   const [currentSunCircumference, setCurrentSunCircumference] = useState();
 
-  const [click, setClick] = useState(3);
+  const [currentPiValue2, setCurrentPiValue2] = useState();
+  const [currentSunCircumference2, setCurrentSunCircumference2] = useState();
 
-  useEffect(() => {
-    async function getValues() {
-      await Axios.post(
-        `http://localhost:5000/pi-value`, { count: click }
-      )
-        .catch((err) => {
-          console.log(err);
-        })
-        .then((res) => {
-          if (res && res.data) {
-            setCurrentPiValue(res.data.piValue);
-            setCurrentSunCircumference(res.data.circumferenceValue);
-          }
-        });
-    }
-    getValues();
-  }, [click])
+
+  const getValuesV1 = async () => {
+    await Axios.post(
+      `http://localhost:5000/pi-value-v1`
+    )
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((res) => {
+        if (res && res.data) {
+          setCurrentPiValue(res.data.piValue);
+          setCurrentSunCircumference(res.data.circumferenceValue);
+        }
+      });
+  }
+
+  const getValuesV2 = async () => {
+    await Axios.post(
+      `http://localhost:5000/pi-value-v2`
+    )
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((res) => {
+        if (res && res.data) {
+          setCurrentPiValue2(res.data.piValue);
+          setCurrentSunCircumference2(res.data.circumferenceValue);
+        }
+      });
+  }
 
   useEffect(() => {
     async function initialize() {
@@ -35,22 +49,48 @@ function App() {
         `http://localhost:5000`
       )
     }
+
+    setCurrentPiValue(null);
+    setCurrentSunCircumference(null);
+
+    setCurrentPiValue2(null);
+    setCurrentSunCircumference2(null);
+
     initialize();
   }, [])
+
+  useEffect(() => {
+    getValuesV1();
+    getValuesV2();
+  }, [])
+
 
 
   return (
     <ChakraProvider>
 
       <Center id="parent-center-box">
+
         <Flex>
           <Text id="topic-text">Calculator of Circumference of Sun</Text>
         </Flex>
 
-        <FlexBox title="Servers' current known value of Pi" value={currentPiValue} />
-        <FlexBox title="Circumference of the Sun" value={currentSunCircumference} unit="km" />
+        <Grid id="grid-box">
+          <Box className="grid-item-box">
+            <FlexBox title="Servers' current known value of Pi" value={currentPiValue} />
+            <FlexBox title="Circumference of the Sun" value={currentSunCircumference} unit="km" />
 
-        <Button id='btn-grad' onClick={() => setClick(click + 1)}>Get more accurate Pi value</Button>
+            <Button id='btn-grad' onClick={getValuesV1}>Get Pi value decimal point one by one</Button>
+          </Box>
+
+          <Box className="grid-item-box">
+            <FlexBox title="Servers' current known value of Pi" value={currentPiValue2} />
+            <FlexBox title="Circumference of the Sun" value={currentSunCircumference2} unit="km" />
+
+            <Button id='btn-grad' onClick={getValuesV2}>Get increasingly accurate Pi value</Button>
+          </Box>
+        </Grid>
+
       </Center>
 
     </ChakraProvider>
